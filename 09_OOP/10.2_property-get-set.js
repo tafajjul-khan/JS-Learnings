46211da3a3083be7d2799305bb7f4e7ac3b2c537
Tafajjul Khan <tafajjulk607@gmail.com>
2025-10-21 19:26:33 +0530

    get and set methods

---
+++ 09_OOP/10.2_property-get-set.js
@@ -0,0 +1,24 @@
+function User(email, password) {
+  this.email = email;
+  this.password = password;
+
+  Object.defineProperty(this, "email", {
+    get: function () {
+      return this._email.toUpperCase()
+    },
+    set: function (value) {
+      this._email = value
+    },
+  });
+  Object.defineProperty(this, "password", {
+    get: function () {
+      return this._password.toUpperCase();
+    },
+    set: function (value) {
+      this._password = value;
+    },
+  });
+}
+
+const chai = new User("email@chai.com", "chai");
+console.log(chai.email);
